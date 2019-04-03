c39c64e0536b973b3359efa9366d962de31877ba
MariaPaschevska <maria.paschevska@gmail.com>
2019-04-03 16:54:43 +0300

    diagrams, circles added

--- src/app/d3/simple/simple.component.ts
+++ src/app/d3/simple/simple.component.ts
@@ -69,4 +69,104 @@ export class SimpleComponent implements OnInit {
       .attr("fill", "yellow")
       .attr("stroke", "orange");
   }
+  
+  drawDiagram1() {
+    let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+      11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+    d3.select("#diagram1").selectAll("div")
+      .data(dataset)
+      .enter()
+      .append("div")
+      .style("display", "inline-block")
+      .style("width", "20px")
+      .style("margin-right", "2px")
+      .style("background-color", "teal")
+      .style("height", function(d) {
+        let barHeight = d * 5;
+        return barHeight + "px";
+      });
+  }
+
+  drawRect1() {
+    let myData = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+      11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+    let w = 500;
+    let h = 100;
+    let barPadding = 1;
+    let svg = d3.select('#rect1')
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    svg.selectAll("rect")
+      .data(myData)
+      .enter()
+      .append("rect")
+      .attr("x", function(d, i) {
+        return i * (w / myData.length) + (w / myData.length - barPadding) / 2;
+      })
+      .attr("y", function(d) {
+        return h - (d * 4) + 14;
+      })
+      .attr("width", w / myData.length - barPadding)
+      .attr("height", function(d) {
+        return d * 4;
+      })
+      .attr("fill", function(d) {
+        return "rgb(0, 0, " + (d * 10) + ")";
+      });
+
+    svg.selectAll("text")
+      .data(myData)
+      .enter()
+      .append("text")
+      .text(function(d) {
+        return d;
+      })
+      .attr("x", function(d, i) {
+        return i * (w / myData.length) + (w / myData.length - barPadding) / 2;
+      })
+      .attr("y", function(d) {
+        return h - (d * 4) + 14;
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("text-anchor", "middle")
+      .attr("fill", "white")
+      .attr("text-anchor", "middle");
+  }
+
+  drawPoint() {
+    let dataset = [
+      [ 5,     20 ],
+      [ 480,   90 ],
+      [ 250,   50 ],
+      [ 100,   33 ],
+      [ 330,   95 ],
+      [ 410,   12 ],
+      [ 475,   44 ],
+      [ 25,    67 ],
+      [ 85,    21 ],
+      [ 220,   88 ]
+    ];
+    let w = 500;
+    let h = 100;
+
+    let svg = d3.select('#point1')
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    svg.selectAll("circle")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+        return d[0];
+      })
+      .attr("cy", function(d) {
+        return d[1];
+      })
+      .attr("r", 5);
+  }
 }
